4d42e337de086d6238f6b088e256ff949b2fcd8a
Eduard Kononovich <101503726+IbadT@users.noreply.github.com>
2025-05-16 10:51:28 +0300

    for check

--- internal/userService/service.go
+++ internal/userService/service.go
@@ -2,7 +2,6 @@ package userservice
 
 import (
 	"errors"
-	"fmt"
 
 	"github.com/google/uuid"
 )
@@ -59,7 +58,7 @@ func (s *userService) UpdateUser(id uuid.UUID, body UserRequest) (User, error) {
 	if err != nil {
 		return User{}, err
 	}
-	fmt.Println(user) // {0fa9eb1e-550b-45d9-a181-53aef48ca5b9 Add your name in the body 1111111}
+
 	user.Email = body.Email
 	user.Password = body.Password
 	if err := s.repo.UpdateUser(user); err != nil {
